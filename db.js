e59b9bb14afc2d1ac8cfa6d41af4b3bba6a4931a
Ravord <ravord2@gmail.com>
2022-04-11 05:46:39 +0200

    another heroku fix

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const { Client } = require('pg')
 const client = new Client({
-  connectionString: process.env.DB_STRING,
-  ssl: true
+  connectionString: `${process.env.DB_STRING}?sslmode=require`
 })
 client.connect()
   .then(() => {
